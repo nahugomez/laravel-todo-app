e6066859ab8869fccaf46f7cd740e300189eba2d
Nahuel Gomez <nahuelgomezs@outlook.com>
2025-11-28 03:25:40 +0000

    feat: add example tasks

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,11 @@
 <h1>Todo App</h1>
-@isset($name)
-<p>Welcome to Todo App, {{ $name }}</p>
-@endisset
+<div>
+    @isset($tasks)
+        <ul>
+            @foreach ($tasks as $task)
+                <li>{{ $task->title }} - {{ $task->completed ? 'Done' : 'Pending' }} - <a
+                        href="{{ route('task.show', ['id' => $task->id]) }}">Show</a></li>
+            @endforeach
+        </ul>
+    @endisset
+</div>
---
+++ resources/views/task.blade.php
@@ -0,0 +1,7 @@
+<h1>{{ $task->title }}</h1>
+<p>{{ $task->description }}</p>
+<p>{{ $task->long_description }}</p>
+<p>{{ $task->completed ? 'Done' : 'Pending' }}</p>
+<p>{{ $task->created_at }}</p>
+<p>{{ $task->updated_at }}</p>
+<a href="{{ route('task.index') }}">Back to List</a>
