e25cb536fdf42011efcc98446c0eaf2d937f2301
Nahuel Gomez <nahuelgomezs@outlook.com>
2025-11-28 03:34:05 +0000

    feat: add app layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,15 @@
-<h1>Todo App</h1>
-<div>
-    @isset($tasks)
-        <ul>
-            @foreach ($tasks as $task)
-                <li>{{ $task->title }} - {{ $task->completed ? 'Done' : 'Pending' }} - <a
-                        href="{{ route('task.show', ['id' => $task->id]) }}">Show</a></li>
-            @endforeach
-        </ul>
-    @endisset
-</div>
+@extends('layouts.app')
+@section('title', 'List of Tasks')
+@section('content')
+    <h1>Todo App</h1>
+    <div>
+        @isset($tasks)
+            <ul>
+                @foreach ($tasks as $task)
+                    <li>{{ $task->title }} - {{ $task->completed ? 'Done' : 'Pending' }} - <a
+                            href="{{ route('task.show', ['id' => $task->id]) }}">Show</a></li>
+                @endforeach
+            </ul>
+        @endisset
+    </div>
+@endsection
--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -1,7 +1,11 @@
-<h1>{{ $task->title }}</h1>
-<p>{{ $task->description }}</p>
-<p>{{ $task->long_description }}</p>
-<p>{{ $task->completed ? 'Done' : 'Pending' }}</p>
-<p>{{ $task->created_at }}</p>
-<p>{{ $task->updated_at }}</p>
-<a href="{{ route('task.index') }}">Back to List</a>
+@extends('layouts.app')
+@section('title', $task->title)
+@section('content')
+    <h1>{{ $task->title }}</h1>
+    <p>{{ $task->description }}</p>
+    <p>{{ $task->long_description }}</p>
+    <p>{{ $task->completed ? 'Done' : 'Pending' }}</p>
+    <p>{{ $task->created_at }}</p>
+    <p>{{ $task->updated_at }}</p>
+    <a href="{{ route('task.index') }}">Back to List</a>
+@endsection
